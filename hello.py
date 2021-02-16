bfc2450e4fedbc30337e3334f679ff2122f791bf
ConnerDuling <conner.m.duling@gmail.com>
2021-02-15 23:35:48 -0500

    Added basic HTML text and print out. Need input.

--- hello.py
+++ hello.py
@@ -7,10 +7,27 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
 
-    result = diceSummation(2, 1, .5)
+    modifier = 0
+    difficultyValue = 0
+    hiddenDC = difficultyValue - modifier - 1
+
+    #Catches for if the values would cause a result of > 100% or < 0%
+    if hiddenDC > 20:
+        hiddenDC = 21
+    if hiddenDC < 1:
+        hiddenDC = 1
+
+    probOfPureWin = 1 - (1 * hiddenDC / 20)
+
+    result = diceSummation(1, 1, probOfPureWin)
+
+    result *= 100
 
     if request.method == "POST":
-        return render_template('index.html', result=result)
+        return render_template('index.html',
+                                result=round(result, 2),
+                                difficultyValue=difficultyValue,
+                                modifier=modifier)
 
     else:
         return render_template('index.html')
