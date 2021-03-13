2c6c263b63935aa4100414518bbe906f496b7088
ConnerDuling <conner.m.duling@gmail.com>
2021-03-12 19:05:54 -0500

    Added functionality for Adv, Stnd, and DisAdv rolls.

--- hello.py
+++ hello.py
@@ -10,11 +10,15 @@ def hello():
     
 
     modifier = 0
+    rollTypeFlag = ""
     if(request.form.get("dcInput") != None):
         difficultyValue = int(request.form.get("dcInput"))
         modifier = int(request.form.get("modInput"))
+        rollTypeFlag = (request.form.get("rollType"))
     else:
         difficultyValue = 10
+    
+    #Calculates hidden REAL number that needs to be rolled >=
     hiddenDC = difficultyValue - modifier - 1
 
     #Catches for if the values would cause a result of > 100% or < 0%
@@ -25,7 +29,17 @@ def hello():
 
     probOfPureWin = 1 - (1 * hiddenDC / 20)
 
-    result = diceSummation(1, 1, probOfPureWin)
+    #Figures out what type of roll is being made.
+    #I.e.   Advantage, roll two keep highers
+    #       Standard, roll one
+    #       Disadvantage, roll two take the lowest
+
+    if rollTypeFlag == "roll at Advantage":
+        result = diceSummation(2, 1, probOfPureWin)
+    elif rollTypeFlag == "standard roll":
+        result = diceSummation(1, 1, probOfPureWin)
+    else:
+        result = diceSummation(2, 2, probOfPureWin)
 
     result *= 100
 
@@ -34,11 +48,8 @@ def hello():
         return render_template('index.html',
                                 result=round(result, 2),
                                 difficultyValue=difficultyValue,
-                                modifier=modifier)
+                                modifier=modifier,
+                                rollType=rollTypeFlag)
 
     else:
         return render_template('index.html')
-
-# @app.route('/howdy.html')
-# def howdy():
-#     return render_template('howdy.html')
